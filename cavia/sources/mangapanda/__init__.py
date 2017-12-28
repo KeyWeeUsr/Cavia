2bbc62173fb2c8d68e58abb682c53c9752b0d95b
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-12-28 15:49:41 +0100

    Add item contents listing for MangaPanda Source

--- cavia/sources/mangapanda/__init__.py
+++ cavia/sources/mangapanda/__init__.py
@@ -7,7 +7,7 @@ from ast import literal_eval
 class MangaPanda(Source):
     name = 'mangapanda'
     url = 'http://www.mangapanda.com'
-    item_url = 'http://www.mangapanda.com/alphabetical'
+    content_url = 'http://www.mangapanda.com/alphabetical'
     language = 'en'
 
     def fetch_list(self):
@@ -36,4 +36,45 @@ class MangaPanda(Source):
                     }
 
         self.write_cache_list(str(items).encode('utf-8'))
+        return items
+
+    def fetch_item(self, item_name):
+        '''Use Source.fetch_item to download item content and parse it.
+        '''
+        super(MangaPanda, self).fetch_item(item_name)
+
+        cache_item = self.cache_item_list(item_name)
+        with open(cache_item, 'rb') as f:
+            cache_item = f.read()
+
+        if cache_item:
+            return literal_eval(cache_item.decode('utf-8'))
+
+        parsed = self.parsed_item
+        items = {}
+
+        i = 0
+        for results in parsed.body.find_all('div', {'id': 'chapterlist'}):
+            for result in results.find_all('table', {'id': 'listing'}):
+                for res in result.find_all('tr', {'class': ''}):
+                    i += 1
+                    res = res.find('td')
+                    a_tag = res.find('a')
+
+                    # strip 'NAME ' from the number
+                    key = a_tag.text.strip(item_name + ' ')
+                    item_url = a_tag.get('href')
+                    assert str(i) == key
+
+                    # MP separator for URL + chapter name
+                    assert ' : ' == res.contents[-1][:3]
+                    text = res.contents[-1][3:]
+                    items[key] = {
+                        'i': i, 'url': self.url + item_url, 'text': text
+                    }
+
+        self.write_cache_item_list(
+            item_name,
+            str(items).encode('utf-8')
+        )
         return items
