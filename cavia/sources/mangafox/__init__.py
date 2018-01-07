6b62b233d9c7f8c4bd4e37f9f78ef42db31555f4
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-01-08 00:19:24 +0100

    Add item contents listing for MangaFox Source

--- cavia/sources/mangafox/__init__.py
+++ cavia/sources/mangafox/__init__.py
@@ -50,3 +50,46 @@ class MangaFox(Source):
 
         self.write_cache_list(str(items).encode('utf-8'))
         return items
+
+    def fetch_item(self, item_name):
+        '''Use Source.fetch_item to download item content and parse it.
+        '''
+        super(MangaFox, self).fetch_item(item_name)
+
+        cache_item = self.cache_item_list(item_name)
+        with open(cache_item, 'rb') as f:
+            cache_item = f.read()
+
+        if cache_item:
+            return literal_eval(cache_item.decode('utf-8'))
+
+        parsed = self.parsed_item
+        items = {}
+
+        i = 0
+        for results in parsed.body.find_all('div', {'id': 'chapters'}):
+            for result in reversed(results.find_all('ul', 'chlist')):
+                for res in reversed(results.find_all('li')):
+                    i += 1
+
+                    a_tag = res.find('a', 'tips')
+                    title = res.find('span', 'title')
+                    text = title.text if title else ''
+
+                    # strip 'NAME ' from the number
+                    key = a_tag.text.strip(item_name + ' ')
+                    item_url = a_tag.get('href')
+
+                    bad_url = '//' + self.url[11:]
+                    if item_url.startswith(bad_url):
+                        item_url = item_url.replace(bad_url, '')
+
+                    items[key] = {
+                        'i': i, 'url': self.url + item_url, 'text': text
+                    }
+
+        self.write_cache_item_list(
+            item_name,
+            str(items).encode('utf-8')
+        )
+        return items
