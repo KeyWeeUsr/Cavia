7a32ea8c45789ad6f3328054fe8ec89cbf4d0322
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-01-19 19:17:04 +0100

    Add items listing for MangaHere Source

--- cavia/sources/mangahere/__init__.py
+++ cavia/sources/mangahere/__init__.py
@@ -1,5 +1,8 @@
 from cavia.sources import Source
 
+from os.path import dirname, join
+from ast import literal_eval
+
 
 class MangaHere(Source):
     name = 'mangahere'
@@ -7,3 +10,40 @@ class MangaHere(Source):
     content_url = 'http://www.mangahere.cc/mangalist/'
     language = 'en'
     extension = '.jpg'
+
+    def fetch_list(self):
+        '''Use Source.fetch_list to download page content and parse it per
+        separate Source file.
+        '''
+        super(MangaHere, self).fetch_list()
+
+        cache_list = self.cache_list()
+        with open(cache_list, 'rb') as f:
+            cache_items = f.read()
+
+        if cache_items:
+            return literal_eval(cache_items.decode('utf-8'))
+
+        parsed = self.parsed
+        items = {}
+
+        i = 0
+        for lists in parsed.body.find_all('div', 'list_manga'):
+            for results in lists.find_all('ul'):
+                for result in results.find_all('li'):
+                    for res in result.find_all('a'):
+                        i += 1
+
+                        # clean messed up URL
+                        res_url = res.get('href')
+                        bad_url = '//' + self.url[7:]
+                        if res_url.startswith(bad_url):
+                            res_url = res_url.replace(bad_url, '')
+
+                        items[res.text] = {
+                            'i': i,
+                            'url': self.url + res_url
+                        }
+
+        self.write_cache_list(str(items).encode('utf-8'))
+        return items
