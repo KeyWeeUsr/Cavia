77c048426cbc47b54a9c3bb2097cdcec454f8f63
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-01-21 23:41:26 +0100

    Add item contents listing for MangaHere Source

--- cavia/sources/mangahere/__init__.py
+++ cavia/sources/mangahere/__init__.py
@@ -47,3 +47,46 @@ class MangaHere(Source):
 
         self.write_cache_list(str(items).encode('utf-8'))
         return items
+
+    def fetch_item(self, item_name):
+        '''Use Source.fetch_item to download item content and parse it.
+        '''
+        super(MangaHere, self).fetch_item(item_name)
+
+        cache_item = self.cache_item_list(item_name)
+        with open(cache_item, 'rb') as f:
+            cache_item = f.read()
+
+        if cache_item:
+            return literal_eval(cache_item.decode('utf-8'))
+
+        parsed = self.parsed_item
+        items = {}
+
+        i = 0
+        for results in parsed.body.find_all('div', 'detail_list'):
+            for result in results.find_all('ul', {'class': None}):
+                for res in reversed(result.find_all('li')):
+                    i += 1
+
+                    a_tag = res.find('a')
+                    text = str(res.find('span', 'left').contents[-1])
+                    text = '' if '<span' in text else text
+
+                    # strip 'NAME ' from the number
+                    key = a_tag.text.strip(item_name + ' ')
+                    item_url = a_tag.get('href')
+
+                    bad_url = '//' + self.url[7:]
+                    if item_url.startswith(bad_url):
+                        item_url = item_url.replace(bad_url, '')
+
+                    items[str(i)] = {
+                        'i': i, 'url': self.url + item_url, 'text': text
+                    }
+
+        self.write_cache_item_list(
+            item_name,
+            str(items).encode('utf-8')
+        )
+        return items
