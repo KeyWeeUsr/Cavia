54c70682446b970206d9bb8030deeea5cf13d3f3
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-01-27 22:52:19 +0100

    Add Source item downloading for MangaStream Source

--- cavia/sources/mangastream/__init__.py
+++ cavia/sources/mangastream/__init__.py
@@ -2,6 +2,9 @@ from cavia.sources import Source
 
 from ast import literal_eval
 
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+
 
 class MangaStream(Source):
     name = 'mangastream'
@@ -77,3 +80,52 @@ class MangaStream(Source):
             str(items).encode('utf-8')
         )
         return items
+
+    def download_item(self, item_name, start, end):
+        '''Use Source.download_item to parse download urls
+        and retrieve the content.
+        '''
+        super(MangaStream, self).download_item(item_name, start, end)
+        all_urls = []
+
+        for part, chap in self.downloading:
+            cache_download = self.cache_download_list(item_name, part)
+            with open(cache_download, 'rb') as f:
+                cache_download = f.read()
+
+            if not cache_download:
+                controls = chap.find_all('div', 'controls')[0]
+                pagination = controls.find_all('ul', 'dropdown-menu')[-1]
+                links = []
+                for page in pagination.find_all('a'):
+                    request = Request(
+                        self.url + page.get('href'),
+                        headers={
+                            'User-Agent' : (
+                                'Mozilla/5.0 '
+                                '(Windows NT 6.3; Win64; x64; rv:57.0) '
+                                'Gecko/20100101 Firefox/57.0'
+                            )
+                        }
+                    )
+                    website = urlopen(
+                        request,
+                        timeout=self.connection_timeout
+                    )
+                    content = BeautifulSoup(
+                        website.read().decode('utf-8'), 'html.parser'
+                    )
+
+                    found = content.find_all('img', {'id': 'manga-page'})[0]
+                    src = found['src']
+                    if src[:2] == '//':
+                        src = src.replace('//', 'http://')
+                    links.append(src)
+                self.write_cache_download_list(
+                    item_name, part,
+                    str(links).encode('utf-8')
+                )
+            else:
+                links = literal_eval(cache_download.decode('utf-8'))
+            all_urls.append([part, links])
+        self.download_files(self.download_folder, all_urls)
